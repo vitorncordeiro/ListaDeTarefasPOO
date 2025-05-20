615a95a82ca2a27e286dcf085457bbac1b45c273
Vitor Natal Cordeiro <111884078+vitorncordeiro@users.noreply.github.com>
2025-05-20 11:51:38 -0300

    Update main.py

--- main.py
+++ main.py
@@ -11,30 +11,33 @@ class Tarefa:
     def __call__(self, *args, **kwds):
         
         return [self.titulo, self.descricao, self.status, self.data]
-    #função para adicionar um novo item à lista, salvando essa ação no dict ultimaAcao
+    #função para adicionar um novo item à lista
     def adicionar_item(self):
-        global ultimaAcao
+        
         listaAtt = [self.titulo, self.descricao, self.status, self.data]
         listaTarefas.append(listaAtt)
-        ultimaAcao[f'adicionado_{self.titulo}'] = self.titulo
+        
         return listaTarefas
-    #função para excluir um item da lista, e salva a execução dessa ação no dict ultimaAcao
+    #função para excluir um item da lista
+
+
     def excluir_item(self, titulo):
         try:
-            global ultimaAcao
+            
             for listas in listaTarefas:
-                listas.pop(listas.index(titulo))
-            ultimaAcao[f'excluido_{self.titulo}'] = titulo
+                listaTarefas.pop(listas.index(titulo))
+            
             return listaTarefas
         except ValueError: #o value error é para se a pessoa tentar excluir uma tarefa que não existe na lista dela
             print('Este item não pode ser excluído, pois ele não existe')
-    
+instanciaTarefa = Tarefa(None, None, None, None)   
 while True: #dentro do loop vai perguntar oq o usuário quer fazer e chamar as devidas funções. Caso o usuário queira parar, usa o S para quebrar o laço e conseguir salvar o json no bloco abaixo
     ComandoInicial = input('\nSelecione um comando:\n[L]istar [A]dicionar [E]xcluir [S]air \n\n').upper()
     if ComandoInicial.startswith('S'):
         break
     elif ComandoInicial.startswith('L'):
-        print(f'{listaTarefas}')
+        for lista in listaTarefas:
+            print(lista)
     elif ComandoInicial.startswith('A'):
         titulo = input('Digite o título do item que será adicionado:\n')
         
@@ -47,13 +50,13 @@ while True: #dentro do loop vai perguntar oq o usuário quer fazer e chamar as d
         
         instanciaTarefa = Tarefa(titulo, descricao, status, data)
         listaTarefas.append(instanciaTarefa())
-        instanciaTarefa()
-        print(f'\nAqui está a lista atualizada:\n{listaTarefas}')
 
     elif ComandoInicial.startswith('E'):
-
-        tarefaASerExcluida = input('Digite o item que você deseja excluir:\n')
-        Tarefa.excluir_item(tarefaASerExcluida)
+        
+        tarefaASerExcluida = input('Digite o título do item que você deseja excluir:\n')
+        for listas in listaTarefas:
+            if listas[0] == tarefaASerExcluida:
+                instanciaTarefa.excluir_item(tarefaASerExcluida)
         print(f'\nAqui está a lista atualizada:\n{listaTarefas}')
 
     elif ComandoInicial == 'CLEAR':
